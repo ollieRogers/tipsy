90d67e5fd5686980b7e1b8152755f0edb11cb7b2
Ollie Rogers <ollierogers@joincatapult.com>
2020-04-21 15:16:57 +0100

    remove mock state

--- src/store/initialState.ts
+++ src/store/initialState.ts
@@ -1,31 +1,9 @@
 import { StateShape } from '../types'
 
 export const initialState: StateShape = {
-  people: [
-    {
-      name: 'Ollie',
-      id: 'person-id-25',
-    },
-    {
-      name: 'Sarah',
-      id: 'person-id-26',
-    },
-  ],
+  people: [],
   bill: {
-    items: [
-      {
-        name: 'pizza',
-        id: 'bill-item-id-25',
-        price: 14.99,
-        personId: 'person-id-25',
-      },
-      {
-        name: 'coffee',
-        id: 'bill-item-id-26',
-        price: 13.99,
-        personId: '',
-      },
-    ],
+    items: [],
   },
   serviceCharge: 10,
 }
